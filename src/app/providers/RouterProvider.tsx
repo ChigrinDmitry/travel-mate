052e58366a2c2d9730c912377f7bb3a214b5408f
Dmitry Chigrin <>
2026-02-07 20:01:14 +0500

    feat: implement routing

---
+++ src/app/providers/RouterProvider.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Landing } from '../../pages/landing/ui';
+//import { Dashboard } from '../../pages/dashboard/ui';
+//import { TripPlanning } from '../../pages/trip-planning/ui';
+
+export const RouterProvider = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                {/* <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/trip/:tripId" element={<TripPlanning />} /> */}
+            </Routes>
+        </BrowserRouter>
+    );
+};
